c71175d2e3a76d4a18821adc645e82a258c0f470
Alexander Hernández Herrera <aherrera@itcoint.com>
2021-04-16 17:17:48 -0600

    Agregar archivos de proyecto.

---
+++ queue-send/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace queue_send
+{
+    class Program
+    {
+        private static string _bus_connectionstring= "Endpoint=sb://prueba456.servicebus.windows.net/;SharedAccessKeyName=policyxd;SharedAccessKey=Vzl+ULjwEWBPd+V54gfSvhCxOGt4GRupsbc3aZBOTyc=";
+        private static string _queue_name = "appqueueprueba";
+        static async Task Main(string[] args)
+        {
+            IQueueClient _client;
+            _client = new QueueClient(_bus_connectionstring, _queue_name);
+            Console.WriteLine("Sending Messages");
+            for (int i = 1; i <=10; i++)
+            {
+                Order obj = new Order();
+                var _message = new Message(Encoding.UTF8.GetBytes(obj.ToString()));
+                await _client.SendAsync(_message);
+                Console.WriteLine($"Sending Message : {obj.Id} ");
+            }
+        }
+        }
+}
